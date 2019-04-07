7772e90bdf0c9a40f306d5045d3dadec385b0f68
ferlete <32394631+ferlete@users.noreply.github.com>
2019-04-07 18:50:54 -0400

    Update p2p.py
    new parameters

--- P2P/p2p.py
+++ P2P/p2p.py
@@ -4,63 +4,59 @@ __version__ = "0.1"
 import sys
 import argparse
 import threading
-import time
-from random import randint
 
 # import from project P2P
 from .info import Info
 from .server import Server
 from .client import Client
-from .fileIO import FileIO
 
+list_hosts_peers = []  # list of hosts servers
+list_port_peers = []  # list of ports servers
 
-list_hosts_peers = [] # list of hosts servers
-list_port_peers = [] # list of ports servers
-
-BYTE_SIZE = 1024
 PEER_BYTE_DIFFERENTIATOR = b'\x11'
 RAND_TIME_START = 1
 RAND_TIME_END = 2
-REQUEST_STRING = "req"
 
- # make ourself the default peer
-peers = ['127.0.0.1:8889']
 
 def main():
-
-    default_port = 8889
+    default_port = 9999
     server_ip = '127.0.0.1'
     info = Info('Andre, Patrik e Valter', 'ferlete@gmail.com')
+    msg = "Server Hello"
+    transport = "TCP"
 
     parser = argparse.ArgumentParser(description='P2P tester')
-    parser.add_argument('--type', '-t', help='choice server or client', default='server')  # server or client
-    parser.add_argument('--port', '-p', help='server port', type=int, default=default_port)  # port Server
-    parser.add_argument('--version', '-v', action='version', version='P2P ' + __version__) # show version
+    parser.add_argument('--type', '-t', dest="type", help='choice server or client',
+                        default='server')  # server or client
+    parser.add_argument('--tcp', dest="tcp", action='store_true', help='use TCP for transport', default=True,
+                        required=False)  # User default TCP for transport
+    parser.add_argument('--udp', dest="udp", action='store_true', help='use UDP for transport', default=False,
+                        required=False)  # User default TCP for transport
+    parser.add_argument('--port', '-p', dest="port", help='server port', type=int, default=default_port)  # port Server
+    parser.add_argument('--version', '-v', action='version', version='P2P tester version ' + __version__)  # show version
     parser.add_argument('--debug', help='increase output verbosity')
 
     args = parser.parse_args()
-    file = FileIO
-    msg = file.convert_to_bytes()
+    if args.tcp:
+        transport = "TCP"
+    if args.udp:
+        transport = "UDP"
 
-    if args.type == 'server':
-        # become the server
-        try:
-            Server(server_ip, args.port, msg)
-        except KeyboardInterrupt:
-            sys.exit()
+    print("P2P tester version " + __version__)
+    print(info.get_authors())
 
-    if args.type == 'client':
-        print("-" * 21 + "Trying to connect" + "-" * 21)
-        for peer in peers:
-            print(peer)
-            try:
-                ip, port = peer.strip().split(':')
-                Client(ip, port)
+    try:
+        if args.type == 'server':
+            Server(server_ip, args.port, transport, msg)
 
-            except KeyboardInterrupt:
-                sys.exit(0)
-            except:
-                pass
+        if args.type == 'client':
+            loadpeers()
+            #for i in range(len(list_hosts_peers)):
+            Client(str(list_hosts_peers[0]), int(list_port_peers[0]))
+
+
+    except Exception as ex:
+        print(ex)
 
 
 def loadpeers():
